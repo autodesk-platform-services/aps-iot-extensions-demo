c4c9c3e42ae86dc56a54c2c80d1f167caefa298a
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-02-10 15:14:57 +0100

    Use dotenv to access env variables

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+
 let { APS_CLIENT_ID, APS_CLIENT_SECRET, PORT } = process.env;
 if (!APS_CLIENT_ID || !APS_CLIENT_SECRET) {
     console.warn('Missing some of the environment variables.');
