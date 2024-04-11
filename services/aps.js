100096c3589b331aea6b9f27217ef539abfc71f1
Petr Broz <petr.broz@autodesk.com>
2024-04-11 15:59:34 +0200

    Moved to official APS SKD.

--- services/aps.js
+++ services/aps.js
@@ -1,13 +1,17 @@
-const { AuthClientTwoLegged } = require('forge-apis');
+const { SdkManagerBuilder } = require('@aps_sdk/autodesk-sdkmanager');
+const { AuthenticationClient, Scopes } = require('@aps_sdk/authentication');
 const { APS_CLIENT_ID, APS_CLIENT_SECRET } = require('../config.js');
 
-let publicAuthClient = new AuthClientTwoLegged(APS_CLIENT_ID, APS_CLIENT_SECRET, ['viewables:read'], true);
+const sdkManager = SdkManagerBuilder.create().build();
+const authenticationClient = new AuthenticationClient(sdkManager);
 
+let _credentials = null;
 async function getPublicToken() {
-    if (!publicAuthClient.isAuthorized()) {
-        await publicAuthClient.authenticate();
+    if (!_credentials || _credentials.expires_at < Date.now()) {
+        _credentials = await authenticationClient.getTwoLeggedToken(APS_CLIENT_ID, APS_CLIENT_SECRET, [Scopes.ViewablesRead]);
+        _credentials.expires_at = Date.now() + _credentials.expires_in * 1000;
     }
-    return publicAuthClient.getCredentials();
+    return _credentials;
 }
 
 module.exports = {
